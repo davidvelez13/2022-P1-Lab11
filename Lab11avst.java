01e3c11e89bee52c243f9cbea4aae87f96d8b29a
21377 <t@vtsd.com>
2023-01-05 09:24:02 -0500

    ty11

--- Lab11avst.java
+++ Lab11avst.java
@@ -12,9 +12,9 @@ public class Lab11avst
     {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
-        final int MAX = 100;
-        boolean primes[];
-        primes = new boolean[MAX];
+        System.out.print("Enter the primes upperbound ===>> ");
+        final int MAX = input.nextInt();
+        boolean primes[] = new boolean[MAX];
         computePrimes(primes);
         displayPrimes(primes);
     }
@@ -22,20 +22,20 @@ public class Lab11avst
     public static void computePrimes(boolean primes[])
     {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
-        int prime[] = new int[100];
 
-        for(int k = 2; k < prime.length; k++){
-            primes[k] = true;}
+        for(int k = 2; k< primes.length; k++){
+            primes[k] = true;
 
-            for(int x =4; x< primes.length; x+=2){
-                primes[x] = false;
+    }
+
+
+        for(int index = 2; index < primes.length; index++){
+            for(int k = index*2; k< primes.length; k+=index){
+                primes[k] = false;
+
+
             }
-
-            for(int y = 6; y < primes.length; y+=3){
-                primes[y] = false;
-            }
-
-            for(int)
+        }
 
 
     }
@@ -44,8 +44,6 @@ public class Lab11avst
     public static void displayPrimes(boolean primes[]) {
         System.out.println("\n\nPRIMES BETWEEN 1 AND " + primes.length);
         System.out.println();
-
-
         for (int k = 2; k < primes.length; k++) {
             if (primes[k] == true) {
                 if (primes[k] == true) {
